44634842059e507f9f4215aff02c510b056418dd
Aliia <aliia@MacBook-Pro-Aliia.local>
2022-06-15 15:59:33 +0600

    yesss

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Header = ({logo, cars}) => {
+    // console.log(logo);
+    // console.log(cars);
+    return (
+        <div style={{
+            display: 'flex',
+            justifyConetnt: 'space-between',
+            alignItems: 'center'
+        }}>
+            <img src={logo} alt="" width='200px' />
+            <Navbar cars={cars} />
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/List/List.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Card from '../Card/Card';
+
+const List = ({ cars }) => {
+    const [activeCard, setActiveCard] = useState('')
+    // console.log(cars);
+    return (
+        <div 
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+                }}>
+                {cars.map(item => (
+                    // <span key={item.id}>{item.model}</span>
+                    <Card
+                    key={item.id}
+                    item={item}
+                    activeCard={activeCard}
+                    setActiveCard={setActiveCard} />
+                ))}
+        </div>
+    );
+};
+
+export default List;
